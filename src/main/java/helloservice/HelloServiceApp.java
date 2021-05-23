4108fb14372c752a33689837ee1aefaf4cbc71d7
Afshan Ahmed <afshanahmed.ec18@rvce.edu.in>
2021-05-23 18:59:19 +0530

    Modified

--- src/main/java/helloservice/HelloServiceApp.java
+++ src/main/java/helloservice/HelloServiceApp.java
@@ -1,46 +1,25 @@
 package helloservice;
 
-import com.slack.api.Slack;
 import com.slack.api.bolt.App;
-import com.slack.api.bolt.context.builtin.SlashCommandContext;
-import com.slack.api.bolt.handler.builtin.SlashCommandHandler;
 import com.slack.api.bolt.jetty.SlackAppServer;
-import com.slack.api.bolt.request.builtin.SlashCommandRequest;
-import com.slack.api.bolt.response.Response;
-import com.slack.api.methods.MethodsClient;
-import com.slack.api.methods.SlackApiException;
-import com.slack.api.methods.request.chat.ChatPostMessageRequest;
-import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 
-import java.io.IOException;
 
 public class HelloServiceApp
 {
-    public static void main(String[] args) throws SlackApiException, IOException {
-        String token = "xoxb-2113917763184-2090639650883-IuS55wnEr7pBO1x50zrnXyei";
-        Slack slack = Slack.getInstance();
-        MethodsClient methods = slack.methods(token);
-        ChatPostMessageRequest request = ChatPostMessageRequest.builder().channel("#random").text(":wave: Hi there!").build();
-        try
+    public static void main(String[] args)
         {
-            ChatPostMessageResponse response = methods.chatPostMessage(request);
-            System.out.println(response);
+            App app = new App();
+            AppSlashCommandHandler appSlashCommandHandler = new AppSlashCommandHandler();
+            app.command("/hi", appSlashCommandHandler);
+            SlackAppServer server = new SlackAppServer(app, 8080);
+            try {
+                server.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
-        catch (Exception e)
-        {
-            System.out.println("there is an error!");
-        }
-        App app = new App();
-        AppSlashCommandHandler appSlashCommandHandler = new AppSlashCommandHandler();
-        app.command("/hi", appSlashCommandHandler);
-        SlackAppServer slackAppServer = new SlackAppServer(app, "/slack/events", 8080);
-        try {
-            slackAppServer.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+
 
     }
-
-
-}
