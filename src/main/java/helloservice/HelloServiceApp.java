5451fde00ec27a700d550fb98ac87cd540e0c0b7
Afshan Ahmed <afshanahmed.ec18@rvce.edu.in>
2021-05-23 13:14:01 +0530

    added some more code

--- src/main/java/helloservice/HelloServiceApp.java
+++ src/main/java/helloservice/HelloServiceApp.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 public class HelloServiceApp
 {
     public static void main(String[] args) throws SlackApiException, IOException {
-        String token = "xoxb-2113917763184-2090639650883-dVg6YFNipPEL0G4xVCoAiGtP";
+        String token = "xoxb-2113917763184-2090639650883-IuS55wnEr7pBO1x50zrnXyei";
         Slack slack = Slack.getInstance();
         MethodsClient methods = slack.methods(token);
         ChatPostMessageRequest request = ChatPostMessageRequest.builder().channel("#random").text(":wave: Hi there!").build();
