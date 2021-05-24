37b6dc98c556cd177c42bb554326c4b49b86c801
Afshan Ahmed <afshanahmed.ec18@rvce.edu.in>
2021-05-24 08:51:48 +0530

    .

--- src/main/java/helloservice/HelloServiceApp.java
+++ src/main/java/helloservice/HelloServiceApp.java
@@ -1,7 +1,14 @@
 package helloservice;
 
 import com.slack.api.bolt.App;
+import com.slack.api.bolt.context.builtin.SlashCommandContext;
+import com.slack.api.bolt.handler.builtin.SlashCommandHandler;
 import com.slack.api.bolt.jetty.SlackAppServer;
+import com.slack.api.bolt.request.builtin.SlashCommandRequest;
+import com.slack.api.bolt.response.Response;
+import com.slack.api.methods.SlackApiException;
+
+import java.io.IOException;
 
 
 public class HelloServiceApp
@@ -9,9 +16,14 @@ public class HelloServiceApp
     public static void main(String[] args)
         {
             App app = new App();
-            AppSlashCommandHandler appSlashCommandHandler = new AppSlashCommandHandler();
-            app.command("/hi", appSlashCommandHandler);
-            SlackAppServer server = new SlackAppServer(app, 8080);
+          //  AppSlashCommandHandler appSlashCommandHandler = new AppSlashCommandHandler();
+            app.command("/hi", new SlashCommandHandler() {
+                @Override
+                public Response apply(SlashCommandRequest slashCommandRequest, SlashCommandContext context) throws IOException, SlackApiException {
+                    return context.ack("Hey!");
+                }
+            });
+            SlackAppServer server = new SlackAppServer(app);
             try {
                 server.start();
             } catch (Exception e) {
